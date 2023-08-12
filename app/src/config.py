d54114e07f8dd60827b69a934f69404608319db6
mikhail khaykin <mkhaykin@outlook.com>
2023-08-12 22:27:05 +0300

    celery import

--- app/src/config.py
+++ app/src/config.py
@@ -2,14 +2,20 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
-    POSTGRES_HOST: str = ''
-    POSTGRES_PORT: int = 0
+    POSTGRES_HOST: str = '127.0.0.1'
+    POSTGRES_PORT: int = 5432
     POSTGRES_DB: str = ''
     POSTGRES_USER: str = ''
     POSTGRES_PASSWORD: str = ''
 
-    REDIS_SERVER: str = ''
-    REDIS_PORT: int = 0
+    REDIS_SERVER: str = '127.0.0.1'
+    REDIS_PORT: int = 6379
+
+    RABBITMQ_SERVER: str = '127.0.0.1'
+    RABBITMQ_PORT: int = 5672
+    RABBITMQ_DEFAULT_USER: str = ''
+    RABBITMQ_DEFAULT_PASS: str = ''
+    RABBITMQ_DEFAULT_VHOST: str = ''
 
     model_config = SettingsConfigDict(env_file='.env')
 
---
+++ app/src/task/__init__.py
@@ -0,0 +1,3 @@
+from .conn import app_celery
+import app.src.task.conn
+# from . import tasks
